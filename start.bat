aa74049b208d8671720fdb58c18cbceb8c70ef22
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2024-12-31 19:30:05 +0300

    1

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 #git checkout -- package-lock.json
-#git pull
-export NPM_CONFIG_YES=true
-npm install --legacy-peer-deps --force --yes
+git pull
+#export NPM_CONFIG_YES=true
+#npm install --legacy-peer-deps --force --yes
 npm start run
 #rm -vrf logs
