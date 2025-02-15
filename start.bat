b9645c2ce55e416c8e6082fbc187ea985b257728
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2025-02-15 17:50:03 +0300

    npm install

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ echo ---------------------- START (%DATE:~0,2%.%DATE:~3,2% %TIME:~0,8%) --------
 #git checkout -- package-lock.json
 git pull
 #export NPM_CONFIG_YES=true
-#npm install --legacy-peer-deps --force --yes
+npm install --legacy-peer-deps --force --yes
 npm start run
 rm -vrf logs
 rm -vrf maps
