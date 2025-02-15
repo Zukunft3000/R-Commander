81f5a0a7d8f7631aa6ad6d63f5e7e379253c50b0
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2025-02-15 15:12:38 +0300

    restart,github comm

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
+:restart
+echo ---------------------- START (%DATE:~0,2%.%DATE:~3,2% %TIME:~0,8%) ----------------------
 #git checkout -- package-lock.json
 git pull
 #export NPM_CONFIG_YES=true
@@ -5,3 +7,6 @@ git pull
 npm start run
 rm -vrf logs
 rm -vrf maps
+echo ---------------------- STOP  (%DATE:~0,2%.%DATE:~3,2% %TIME:~0,8%) ----------------------
+sleep 5
+goto restart
